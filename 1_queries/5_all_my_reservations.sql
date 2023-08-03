c04616fa09207de2658d55e8fe197418d2d15a0a
Afreeda <64160666+afreeda-m@users.noreply.github.com>
2023-08-03 09:10:37 -0400

    edit 5_all_my_reservations to remove a JOIN

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -1,9 +1,8 @@
-SELECT reservations.id as id, properties.title as title, properties.cost_per_night as cost_per_night, reservations.start_date as start_date, avg(property_reviews.rating) as average_rating
-FROM property_reviews
-JOIN properties ON property_reviews.property_id = properties.id
-JOIN reservations ON properties.id = reservations.property_id
-JOIN users ON reservations.guest_id = users.id
-WHERE users.email = 'tristanjacobs@gmail.com'
-GROUP BY reservations.id, properties.title, properties.cost_per_night, reservations.start_date
-ORDER BY start_date
+SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
 LIMIT 10;
